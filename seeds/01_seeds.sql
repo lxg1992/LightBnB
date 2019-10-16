6a26e7adad087c584a327c2bdb9ce5824c283920
Alex Garin <lxg1992@gmail.com>
2019-10-16 20:39:16 +0000

    Custom users

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password) 
+VALUES
+  ('Alex Garin', 'lx92@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('John Bee', 'jb@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Top Kek', 'topkek@yahoo.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Based Man', 'bman@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Rich Man', 'richman@loaded.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
