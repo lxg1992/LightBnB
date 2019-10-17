e77596e84b9101ae724a4add4244e1252f3c91e0
Alex Garin <lxg1992@gmail.com>
2019-10-17 18:44:05 +0000

    Added Search functionality

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,9 +90,62 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-    SELECT * FROM properties
-    LIMIT $1`, [limit])
+
+  let isWhere = true;
+
+  const whereOrAnd = function(where) {
+    if (where) {
+      isWhere = false;
+      return "WHERE";
+    } else {
+      return "AND";
+    }
+  }
+
+  const queryParams = [];
+
+  let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id 
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `${whereOrAnd(isWhere)} city LIKE $${queryParams.length} `;
+  }
+
+  if(options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `${whereOrAnd(isWhere)} owner_id = $${queryParams.length} `;
+  }
+
+  if(options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `${whereOrAnd(isWhere)} cost_per_night >= $${queryParams.length} `; 
+  }
+
+  if(options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `${whereOrAnd(isWhere)} cost_per_night <= $${queryParams.length} `; 
+  }
+
+  if(options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `${whereOrAnd(isWhere)} rating >= $${queryParams.length} `;
+  }
+
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString,queryParams);
+    
+  return pool.query(queryString, queryParams)
     .then(res => {
       return res.rows;
     })
